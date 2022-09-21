0e6cf4ee3282638481966ba980f25066689d88f1
Hendris <hendris.maar@gmail.com>
2022-09-21 11:39:18 +0300

    Add scaffolded controller ChefsController with Create, Delete, Details, Edit and Index views

---
+++ MvcChef/Controllers/ChefsController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using MvcChef.Data;
+using MvcChef.Models;
+
+namespace MvcChef.Controllers
+{
+    public class ChefsController : Controller
+    {
+        private readonly MvcChefContext _context;
+
+        public ChefsController(MvcChefContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Chefs
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Chef.ToListAsync());
+        }
+
+        // GET: Chefs/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var chef = await _context.Chef
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (chef == null)
+            {
+                return NotFound();
+            }
+
+            return View(chef);
+        }
+
+        // GET: Chefs/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Chefs/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,FirstName,LastName,Restaurant")] Chef chef)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(chef);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(chef);
+        }
+
+        // GET: Chefs/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var chef = await _context.Chef.FindAsync(id);
+            if (chef == null)
+            {
+                return NotFound();
+            }
+            return View(chef);
+        }
+
+        // POST: Chefs/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,FirstName,LastName,Restaurant")] Chef chef)
+        {
+            if (id != chef.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(chef);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ChefExists(chef.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(chef);
+        }
+
+        // GET: Chefs/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var chef = await _context.Chef
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (chef == null)
+            {
+                return NotFound();
+            }
+
+            return View(chef);
+        }
+
+        // POST: Chefs/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var chef = await _context.Chef.FindAsync(id);
+            _context.Chef.Remove(chef);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ChefExists(int id)
+        {
+            return _context.Chef.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ MvcChef/Data/MvcChefContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MvcChef.Models;
+
+namespace MvcChef.Data
+{
+    public class MvcChefContext : DbContext
+    {
+        public MvcChefContext (DbContextOptions<MvcChefContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MvcChef.Models.Chef> Chef { get; set; }
+    }
+}
