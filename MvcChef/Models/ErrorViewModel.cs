8d12e22e5e5334aedf14fb3fbccb2f74e275a220
opilane <opilane@HP-CZC6297B8B>
2022-09-21 11:33:49 +0300

    Add project files.

---
+++ MvcChef/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MvcChef.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
