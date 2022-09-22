4bf71f89805eb8c57f0e276d55d25b89bde60138
Hendris <hendris.maar@gmail.com>
2022-09-22 15:12:45 +0300

    Add new model ChefRestaurantViewModel

---
+++ MvcChef/Models/ChefRestaurantViewModel.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcChef.Models
+{
+    public class ChefRestaurantViewModel
+    {
+        public List<Chef> Chefs { get; set; }
+        public SelectList Restaurants { get; set; }
+        public string? ChefRestaurant { get; set; }
+        public string? SearchStringFN { get; set; }
+        public string? SearchStringLN { get; set; }
+    }
+}
